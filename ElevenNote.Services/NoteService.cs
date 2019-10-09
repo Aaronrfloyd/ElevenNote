fe7b38d05b5fe37a0b28cf0c0bfac57dc9ed4b9e
Aaron Floyd <aaronrfloyd@gmail.com>
2019-10-09 16:48:09 -0400

    eight

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -34,14 +34,31 @@ namespace ElevenNote.Services
             }
 
         }
-            public IEnumerable<NoteListItem> GetNotes()
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
             {
-                using (var ctx = new ApplicationDbContext())
-                {
-                    var query = ctx.Notes.Where(e => e.OwnerId == _userId).Select(e => new NoteListItem { NoteId = e.NoteId, Title = e.Title, CreatedUtc = e.CreatedUtc });
-                    return query.ToArray();
-                }
+                var query = ctx.Notes.Where(e => e.OwnerId == _userId).Select(e => new NoteListItem { NoteId = e.NoteId, Title = e.Title, CreatedUtc = e.CreatedUtc });
+                return query.ToArray();
             }
+        }
+
+        public  NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Notes.Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return new NoteDetail
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };
+            }
+        }
+
 
     }
 }
